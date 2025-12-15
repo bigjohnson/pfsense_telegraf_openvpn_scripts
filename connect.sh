f488338fe82b452e476ef6c81cf2cb8f282be13a
Alberto Panu <alberto@panu.it>
2025-12-15 10:37:00 +0100

    Add files via upload

---
+++ connect.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/php -q
+<?php
+
+        require_once("/etc/inc/notices.inc");
+        $local_connect_value = "Openvpn connected from user_name: " . getenv('common_name') . " vpn_client_ip: " . getenv('ifconfig_pool_remote_ip') . " from: " . getenv('trusted_ip') . " on " . date('F j, Y, g:i a');
+        notify_all_remote("$local_connect_value");
+?>
