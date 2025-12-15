a1f5b2d193635ba7b3b1ee948789d45484c61f23
Alberto Panu <alberto@panu.it>
2025-12-15 11:17:13 +0100

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -2,6 +2,7 @@
 <?php
 
         require_once("/etc/inc/notices.inc");
-        $local_connect_value = "Openvpn connected from user_name: " . getenv('common_name') . " vpn_client_ip: " . getenv('ifconfig_pool_remote_ip') . " from: " . getenv('trusted_ip') . " on " . date('F j, Y, g:i a');
+        $local_connect_value = "Openvpn connected from user_name: " . getenv('common_name') . " vpn_client_ip: " . getenv('ifconfig_pool_remote_ip') . " from: " . getenv('trusted_ip') . " on " . date('F j, Y, G:i');
         notify_all_remote("$local_connect_value");
 ?>
+
